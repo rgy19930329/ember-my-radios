84fdd729be699597c693f1649eb280af9b997385
ranguangyu <ranguangyu@zbj.com>
2016-11-24 17:22:05 +0800

    my-radios

---
+++ addon/components/my-radios/component.js
@@ -0,0 +1,44 @@
+import Ember from 'ember';
+import layout from './template';
+
+export default Ember.Component.extend({
+	layout,
+	tagName: 'div',
+    classNames: ['my-radios'],
+	content: [],
+	inputName: '',
+	optionValuePath: '',
+    optionLabelPath: '',
+	value: '',
+	name: '',
+
+	init() {
+		this._super(...arguments);
+	    let str = this.randomString(5);
+        if(!this.get('inputName')){
+            this.set('inputName', str);
+        }
+	},
+
+    actions: {
+        select(index) {
+            let value = this.get('content')[index][this.get('optionValuePath')],
+                name = this.get('content')[index][this.get('optionLabelPath')];
+            this.set('value', value);
+            this.set('name', name);
+        }
+    },
+
+    // 生成随机字符串
+	randomString(len) {　　
+		len = len || 32;　　
+        /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
+		var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';
+		var maxPos = $chars.length;　　
+		var pwd = '';　　
+		for (var i = 0; i < len; i++) {　　　　
+			pwd += $chars.charAt(Math.floor(Math.random() * maxPos));　　
+		}　　
+		return pwd;
+	}
+});
---
+++ app/components/my-radios/component.js
@@ -0,0 +1,1 @@
+export { default } from 'ember-my-radios/components/my-radios/component';
